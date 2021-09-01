8b49a216bd276cb0cd47088d42fecdba64eedddd
jack.crowley <jack.crowley@AFSWW5AC46C350.us.accenturefederal.com>
2021-09-01 08:41:10 -0500

    1.0 Task: Refactored Game_Service_Impl.java into GameServiceImpl.java

--- mgl-webapp/src/main/java/com/organization/mvcproject/MGL_Task1/service/GameServiceImpl.java
+++ mgl-webapp/src/main/java/com/organization/mvcproject/MGL_Task1/service/GameServiceImpl.java
@@ -10,7 +10,7 @@ import com.organization.mvcproject.MGL_Task1.model.Game;
 //TODO 1.0  follow java class naming, improve class name
 //TODO 1.0  "javaGameService" service reference name is not necessary, remove it.
 @Service("javaGameService")
-public class Game_Service_Impl implements GameService {
+public class GameServiceImpl implements GameService {
 
 	/**
 	 * TODO 2.0 The class that interacts with persistent data is called a Data Access Object(DAO)
