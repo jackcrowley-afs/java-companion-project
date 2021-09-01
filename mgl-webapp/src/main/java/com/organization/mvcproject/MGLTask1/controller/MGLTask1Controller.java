1e18adbcbd8e2ae8c25503a38352fdf0c6a1ab2f
jack.crowley <jack.crowley@AFSWW5AC46C350.us.accenturefederal.com>
2021-09-01 08:59:34 -0500

    1.0 Task: Refactored MGL_Task1.service

--- mgl-webapp/src/main/java/com/organization/mvcproject/MGLTask1/controller/MGLTask1Controller.java
+++ mgl-webapp/src/main/java/com/organization/mvcproject/MGLTask1/controller/MGLTask1Controller.java
@@ -14,9 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.organization.mvcproject.MGLTask1.service.GameService;
 import com.organization.mvcproject.MGL_Task1.model.Game;
 import com.organization.mvcproject.MGL_Task1.model.Review;
-import com.organization.mvcproject.MGL_Task1.service.GameService;
 
 
 @Controller
