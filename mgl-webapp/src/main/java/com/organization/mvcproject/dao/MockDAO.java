380a29c8e7051c182c5a1a77769339176e2e86a7
jack.crowley <jack.crowley@AFSWW5AC46C350.us.accenturefederal.com>
2021-09-03 16:19:40 -0500

    2.0 Task:  Add MockDAO Class with respective package.

---
+++ mgl-webapp/src/main/java/com/organization/mvcproject/dao/MockDAO.java
@@ -0,0 +1,18 @@
+package com.organization.mvcproject.dao;
+
+
+/** MockDAO:
+ * 
+ *  2.0 Task:
+ *  
+ *  This class needs to 
+ *  "lift the static List declaration of our data from the Service(GameServiceImpl or Game_Service_Impl) into the MockDAO."
+ * 
+ * 
+ * 
+ */
+public class MockDAO {
+
+	
+	
+}
