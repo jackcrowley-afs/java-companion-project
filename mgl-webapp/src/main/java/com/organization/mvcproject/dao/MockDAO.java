23102d157ab84a5c19af1cd9b3022576791f90b5
jack.crowley <jack.crowley@AFSWW5AC46C350.us.accenturefederal.com>
2021-09-03 16:51:02 -0500

    2.0 Task:  Tested functions for MockDAO.  All working.

--- mgl-webapp/src/main/java/com/organization/mvcproject/dao/MockDAO.java
+++ mgl-webapp/src/main/java/com/organization/mvcproject/dao/MockDAO.java
@@ -1,5 +1,9 @@
 package com.organization.mvcproject.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.organization.mvcproject.model.Game;
 
 /** MockDAO:
  * 
@@ -13,6 +17,57 @@ package com.organization.mvcproject.dao;
  */
 public class MockDAO {
 
+	private static Long gameId = new Long(0);
+	private static Long companyId = new Long(0);
+	private static List<Game> games = new ArrayList<Game>();
+
+	static {
+		games = populateGames();
+	}
+
+	private static List<Game> populateGames() {
+
+		Game game1 = new Game();
+		game1.setId(++gameId);
+		game1.setGenre("Sport");
+		game1.setName("Rocket League");
+
+		Game game2 = new Game();
+		game2.setId(++gameId);
+		game2.setGenre("Shooter");
+		game2.setName("Halo 3");
+
+		Game game3 = new Game();
+		game3.setId(++gameId);
+		game3.setGenre("MMORPG");
+		game3.setName("Runescape");
+
+		games.add(game1);
+		games.add(game2);
+		games.add(game3);
+
+		return games;
+	}
+
 	
+	public static List<Game> getGames()
+	{
+		return games;
+	}
+	
+	public static Long getGameId()
+	{
+		return gameId;
+	}
+	
+	public static Long incrementGameId()
+	{
+		return ++gameId;
+	}
+	
+	public static void addGame(Game game)
+	{
+		games.add(game);
+	}
 	
 }
--- mgl-webapp/src/main/java/com/organization/mvcproject/service/GameServiceImpl.java
+++ mgl-webapp/src/main/java/com/organization/mvcproject/service/GameServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.organization.mvcproject.model.Game;
 
+import com.organization.mvcproject.dao.MockDAO;
 
 @Service
 public class GameServiceImpl implements GameService {
@@ -18,49 +19,23 @@ public class GameServiceImpl implements GameService {
 	 *  Move this list, and methods operating on this list to an appropriately named package and class.
 	 */
 	
-	private static Long gameId = new Long(0);
-	private static Long companyId = new Long(0);
-	private static List<Game> games = new ArrayList<Game>();
-
-	static {
-		games = populateGames();
-	}
-
-	private static List<Game> populateGames() {
-
-		Game game1 = new Game();
-		game1.setId(++gameId);
-		game1.setGenre("Sport");
-		game1.setName("Rocket League");
-
-		Game game2 = new Game();
-		game2.setId(++gameId);
-		game2.setGenre("Shooter");
-		game2.setName("Halo 3");
-
-		Game game3 = new Game();
-		game3.setId(++gameId);
-		game3.setGenre("MMORPG");
-		game3.setName("Runescape");
-
-		games.add(game1);
-		games.add(game2);
-		games.add(game3);
-
-		return games;
-	}
-
+	
+	//retrieveAllGames(): Gets all games on the system.
 	@Override
 	public List<Game> retrieveAllGames() {
-		return games;
+		return MockDAO.getGames();
 	}
 
+	
+	//saveGame: Saves a Game on the system.
 	@Override
 	public Game saveGame(Game game) {
-		game.setId(++gameId);
-		games.add(game);
+		game.setId(MockDAO.incrementGameId());
+		MockDAO.addGame(game);
 		return game;
 	}
+	
+	
 
 
 
