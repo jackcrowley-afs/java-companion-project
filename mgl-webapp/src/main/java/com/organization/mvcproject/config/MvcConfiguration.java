22aed38932d6a1cf7f7df999e82ff78ec5ca9616
jack.crowley <jack.crowley@AFSWW5AC46C350.us.accenturefederal.com>
2021-09-20 11:36:22 -0500

    3.0 Mistake Fix: RestTemplate should not have been private.  Fixed.

--- mgl-webapp/src/main/java/com/organization/mvcproject/config/MvcConfiguration.java
+++ mgl-webapp/src/main/java/com/organization/mvcproject/config/MvcConfiguration.java
@@ -39,9 +39,9 @@ public class MvcConfiguration implements WebMvcConfigurer {
 	}
 	
 
-	//Can this be private?
+	
 	@Bean
-	private RestTemplate restTemplate() {
+	RestTemplate restTemplate() {
 		RestTemplate restTemplate = new RestTemplate();
 		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
 		converter.setObjectMapper(new ObjectMapper());
