0aa2f88b535b910c96eed9d2e4df058a298b416f
jack.crowley <jack.crowley@AFSWW5AC46C350.us.accenturefederal.com>
2021-09-20 10:30:05 -0500

    3.0 Polish: Refactor gameManager to gameService

--- mgl-webapp/src/main/java/com/organization/mvcproject/controller/GameController.java
+++ mgl-webapp/src/main/java/com/organization/mvcproject/controller/GameController.java
@@ -6,21 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.organization.mvcproject.api.model.Game;
-import com.organization.mvcproject.api.model.Review;
 import com.organization.mvcproject.api.service.GameService;
 import com.organization.mvcproject.model.GameImpl;
 
@@ -30,18 +24,18 @@ public class GameController {
 
 	
 	@Autowired
-	private GameService gameManager;
+	private GameService gameService;
 	
 	
 	@GetMapping(value = "/game/")
 	public ResponseEntity<List<Game>> fetchAllGames() {
-		return new ResponseEntity<List<Game>>(gameManager.retrieveAllGames(), HttpStatus.OK);
+		return new ResponseEntity<List<Game>>(gameService.retrieveAllGames(), HttpStatus.OK);
 	}
 
 	
 	@PostMapping(value = "/game/", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Void> createGame(@RequestBody GameImpl game) {
-		gameManager.saveGame(game);
+		gameService.saveGame(game);
 		return new ResponseEntity<>(HttpStatus.CREATED);
 	}
 	
@@ -53,16 +47,24 @@ public class GameController {
 	{
 		Long longId = Long.valueOf(id);
 		
-		Boolean ans = gameManager.deleteGame(longId);
+		Boolean ans = gameService.deleteGame(longId);
 		
 		return new ResponseEntity<Boolean>(ans, HttpStatus.OK);
 	}
 	
+	//TODO: Make this format go root/a/b/c (for path variable system)
+	//Look up @PathParam Annotation
 	@PutMapping(value = "/game/{id},{name},{genre}")
 	public ResponseEntity<Boolean> updateGame(@PathVariable String id, @PathVariable String name, @PathVariable String genre)
 	{
 		Long longId = Long.valueOf(id);
 		
+		
+		//TODO: Use a Service to get an existing game rather than create a new one?
+		//Make a Manager method called findGameByID to get an existing game, then send it back down.
+		
+		
+		//TODO: Move the logic for setting the information into the Service.
 		Game game = new GameImpl();
 		
 		game.setId(longId);
@@ -71,7 +73,7 @@ public class GameController {
 		
 		game.setName(name);
 		
-		Boolean ans = gameManager.updateGame(game);
+		Boolean ans = gameService.updateGame(game);
 		
 		
 		return new ResponseEntity<Boolean>(ans, HttpStatus.OK);
@@ -79,7 +81,7 @@ public class GameController {
 	
 	@GetMapping(value = "/game/genre/{genre}")
 	public ResponseEntity<List<Game>> getGamesByGenre(@PathVariable String genre) {
-		return new ResponseEntity<List<Game>>(gameManager.findGamesByGenre(genre), HttpStatus.OK);
+		return new ResponseEntity<List<Game>>(gameService.findGamesByGenre(genre), HttpStatus.OK);
 	}
 	
 }
